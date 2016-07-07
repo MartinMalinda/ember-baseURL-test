7ee7116fd7013fe22cdb246fa8c1df57921f0698
martinmalinda <malindacz@gmail.com>
2016-07-07 22:54:50 +0200

    update cli to 2.6 and unset baseURL

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'base-url',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
